89eba4ac387038ec8b20c3f94e82d52610674f32
raub <raubtierxxx@gmail.com>
2018-01-15 12:40:57 +0300

    fix window ctor defaults

--- js/window.js
+++ js/window.js
@@ -23,12 +23,12 @@ class Window extends EventEmitter {
 		this._width = opts.width || 800;
 		this._height = opts.height || 600;
 		
-		this._display = opts.display || undefined;
+		this._display = opts.display;
 		this._vsync = opts.vsync ? 1 : 0; // 0 for vsync off
 		
 		this._fullscreen = opts.fullscreen ? true : false;
 		this._decorated = opts.decorated === false ? false : true;
-		this._msaa = opts.msaa ? opts.msaa : 2;
+		this._msaa = opts.msaa === undefined ? 2 : opts.msaa;
 		
 		const attribs = this._fullscreen ? glfw.WINDOW : glfw.FULLSCREEN;
 		
