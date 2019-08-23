bdfa22f7a10827fb82dd26badc7ac77431e95ae6
Luis Blanco <luisblanco1337@gmail.com>
2019-08-23 17:13:02 +0300

    wip glfw 33

--- src/cpp/glfw.cpp
+++ src/cpp/glfw.cpp
@@ -47,6 +47,17 @@ JS_METHOD(init) { NAPI_ENV;
 }
 
 
+JS_METHOD(initHint) { NAPI_ENV;
+	
+	REQ_INT32_ARG(0, hint);
+	REQ_INT32_ARG(1, value);
+	
+	glfwInitHint(hint, value);
+	RET_UNDEFINED;
+	
+}
+
+
 // Cleanup resources
 void deinit() {
 	
@@ -90,6 +101,53 @@ void deinit() {
 }
 
 
+Napi::Object describeMonitor(Napi::Env env, GLFWmonitor *monitor, bool isPrimary) {
+	
+	Napi::Object jsMonitor = Napi::Object::New(env);
+	
+	jsMonitor.Set("is_primary", isPrimary);
+	jsMonitor.Set("index", JS_NUM(i));
+	jsMonitor.Set("name", JS_STR(glfwGetMonitorName(monitor)));
+	
+	int xpos, ypos;
+	glfwGetMonitorPos(monitor, &xpos, &ypos);
+	jsMonitor.Set("pos_x", JS_NUM(xpos));
+	jsMonitor.Set("pos_y", JS_NUM(ypos));
+	
+	int width, height;
+	glfwGetMonitorPhysicalSize(monitor, &width, &height);
+	jsMonitor.Set("width_mm", JS_NUM(width));
+	jsMonitor.Set("height_mm", JS_NUM(height));
+	
+	int xscale, yscale;
+	glfwGetMonitorContentScale(monitor, &xscale, &yscale);
+	
+	const GLFWvidmode *mode = glfwGetVideoMode(monitor);
+	jsMonitor.Set("width", JS_NUM(mode->width));
+	jsMonitor.Set("height", JS_NUM(mode->height));
+	jsMonitor.Set("rate", JS_NUM(mode->refreshRate));
+	
+	int modeCount;
+	const GLFWvidmode *modes = glfwGetVideoModes(monitor, &modeCount);
+	Napi::Array jsModes = Napi::Array::New(env);
+	
+	for (int j = 0; j < modeCount; j++) {
+		
+		Napi::Object jsMode = Napi::Object::New(env);
+		jsMode.Set("width", JS_NUM(modes[j].width));
+		jsMode.Set("height", JS_NUM(modes[j].height));
+		jsMode.Set("rate", JS_NUM(modes[j].refreshRate));
+		
+		// NOTE: Are color bits necessary?
+		jsModes.Set(j, jsMode);
+		
+	}
+	
+	jsMonitor.Set("modes", jsModes);
+	
+}
+
+
 JS_METHOD(terminate) { NAPI_ENV;
 	
 	deinit();
@@ -121,6 +179,20 @@ JS_METHOD(getVersionString) { NAPI_ENV;
 }
 
 
+JS_METHOD(glfwGetError) { NAPI_ENV;
+	
+	const char *err;
+	int code = glfwGetError(&err);
+	
+	if (code != GLFW_NO_ERROR) {
+		RET_STR(err);
+	}
+	
+	RET_NULL;
+	
+}
+
+
 JS_METHOD(getTime) { NAPI_ENV;
 	
 	RET_NUM(glfwGetTime());
@@ -142,55 +214,34 @@ JS_METHOD(setTime) { NAPI_ENV;
 
 JS_METHOD(getMonitors) { NAPI_ENV;
 	
-	int monitor_count, mode_count, xpos, ypos, width, height;
+	int monitorCount;
 	
-	GLFWmonitor **monitors = glfwGetMonitors(&monitor_count);
+	GLFWmonitor **monitors = glfwGetMonitors(&monitorCount);
 	GLFWmonitor *primary = glfwGetPrimaryMonitor();
-	const GLFWvidmode *mode, *modes;
 	
-	Napi::Array js_monitors = Napi::Array::New(env);
+	Napi::Array jsMonitors = Napi::Array::New(env);
 	
-	for (int i = 0; i < monitor_count; i++) {
-		
-		Napi::Object js_monitor = Napi::Object::New(env);
-		js_monitor.Set("is_primary", static_cast<bool>(monitors[i] == primary));
-		js_monitor.Set("index", JS_NUM(i));
-		js_monitor.Set("name", JS_STR(glfwGetMonitorName(monitors[i])));
-		
-		glfwGetMonitorPos(monitors[i], &xpos, &ypos);
-		js_monitor.Set("pos_x", JS_NUM(xpos));
-		js_monitor.Set("pos_y", JS_NUM(ypos));
-		
-		glfwGetMonitorPhysicalSize(monitors[i], &width, &height);
-		js_monitor.Set("width_mm", JS_NUM(width));
-		js_monitor.Set("height_mm", JS_NUM(height));
-		
-		mode = glfwGetVideoMode(monitors[i]);
-		js_monitor.Set("width", JS_NUM(mode->width));
-		js_monitor.Set("height", JS_NUM(mode->height));
-		js_monitor.Set("rate", JS_NUM(mode->refreshRate));
-		
-		modes = glfwGetVideoModes(monitors[i], &mode_count);
-		Napi::Array js_modes = Napi::Array::New(env);
-		
-		for (int j = 0; j < mode_count; j++) {
-			
-			Napi::Object js_mode = Napi::Object::New(env);
-			js_mode.Set("width", JS_NUM(modes[j].width));
-			js_mode.Set("height", JS_NUM(modes[j].height));
-			js_mode.Set("rate", JS_NUM(modes[j].refreshRate));
-			
-			// NOTE: Are color bits necessary?
-			js_modes.Set(JS_NUM(j), js_mode);
-			
-		}
-		
-		js_monitor.Set("modes", js_modes);
-		js_monitors.Set(JS_NUM(i), js_monitor);
-		
+	for (int i = 0; i < monitorCount; i++) {
+		jsMonitors.Set(
+			i,
+			describeMonitor(env, monitors[i], monitors[i] == primary)
+		);
 	}
 	
-	RET_VALUE(js_monitors);
+	RET_VALUE(jsMonitors);
+	
+}
+
+
+JS_METHOD(getPrimaryMonitor) { NAPI_ENV;
+	
+	GLFWmonitor *primary = glfwGetPrimaryMonitor();
+	
+	if ( ! primary ) {
+		RET_NULL;
+	}
+	
+	RET_VALUE(describeMonitor(env, monitor, true));
 	
 }
 
@@ -268,14 +319,25 @@ JS_METHOD(testScene) { NAPI_ENV;
 
 JS_METHOD(windowHint) { NAPI_ENV;
 	
-	REQ_UINT32_ARG(0, target);
-	REQ_UINT32_ARG(1, hint);
+	REQ_UINT32_ARG(0, hint);
+	REQ_UINT32_ARG(1, value);
 	
-	if (target == GLFW_VISIBLE) {
-		hintVisible = hint != 0;
+	if (hint == GLFW_VISIBLE) {
+		hintVisible = value != 0;
 	}
 	
-	glfwWindowHint(target, hint);
+	glfwWindowHint(hint, value);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(windowHintString) { NAPI_ENV; THIS_WINDOW;
+	
+	REQ_UINT32_ARG(0, hint);
+	REQ_STR_ARG(1, value);
+	
+	glfwWindowHintString(hint, value.c_str());
 	RET_UNDEFINED;
 	
 }
@@ -384,11 +446,11 @@ JS_METHOD(createWindow) { NAPI_ENV;
 	
 	GLFWmonitor **monitors = nullptr;
 	GLFWmonitor *monitor = nullptr;
-	int monitor_count;
+	int monitorCount;
 	
 	if (info.Length() >= 5 && monitor_idx >= 0) {
-		monitors = glfwGetMonitors(&monitor_count);
-		if (monitor_idx >= monitor_count) {
+		monitors = glfwGetMonitors(&monitorCount);
+		if (monitor_idx >= monitorCount) {
 			JS_THROW("Invalid monitor");
 			RET_NULL;
 		}
@@ -526,12 +588,68 @@ JS_METHOD(getWindowSize) { NAPI_ENV; THIS_WINDOW;
 }
 
 
+JS_METHOD(getWindowFrameSize) { NAPI_ENV; THIS_WINDOW;
+	
+	int left, top, right, bottom;
+	glfwGetWindowFrameSize(window, &left, &top, &right, &bottom);
+	
+	Napi::Object jsMonitor = Napi::Object::New(env);
+	
+	jsMonitor.Set("left", left);
+	jsMonitor.Set("top", top);
+	jsMonitor.Set("right", right);
+	jsMonitor.Set("bottom", bottom);
+	
+	RET_VALUE(jsMonitor);
+	
+}
+
+
+JS_METHOD(getWindowContentScale) { NAPI_ENV; THIS_WINDOW;
+	
+	float xscale, yscale;
+	glfwGetWindowContentScale(window, &xscale, &yscale);
+	
+	Napi::Object jsMonitor = Napi::Object::New(env);
+	
+	jsMonitor.Set("xscale", xscale);
+	jsMonitor.Set("yscale", yscale);
+	
+	RET_VALUE(jsMonitor);
+	
+}
+
+
 JS_METHOD(setWindowSize) { NAPI_ENV; THIS_WINDOW;
 	
 	REQ_UINT32_ARG(1, w);
 	REQ_UINT32_ARG(2, h);
 	
 	glfwSetWindowSize(window, w, h);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(setWindowSizeLimits) { NAPI_ENV; THIS_WINDOW;
+	
+	REQ_UINT32_ARG(1, minwidth);
+	REQ_UINT32_ARG(2, minheight);
+	REQ_UINT32_ARG(3, maxwidth);
+	REQ_UINT32_ARG(4, maxheight);
+	
+	glfwSetWindowSizeLimits(window, minwidth, minheight, maxwidth, maxheight);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(setWindowAspectRatio) { NAPI_ENV; THIS_WINDOW;
+	
+	USE_UINT32_ARG(1, numer, GLFW_DONT_CARE);
+	USE_UINT32_ARG(2, denom, GLFW_DONT_CARE);
+	
+	glfwSetWindowAspectRatio(window, numer, denom);
 	RET_UNDEFINED;
 	
 }
@@ -558,6 +676,63 @@ JS_METHOD(getWindowPos) { NAPI_ENV; THIS_WINDOW;
 	obj.Set("y", JS_NUM(ypos));
 	
 	RET_VALUE(obj);
+	
+}
+
+
+JS_METHOD(getWindowOpacity) { NAPI_ENV; THIS_WINDOW;
+	
+	float opacity = glfwGetWindowOpacity(window);
+	RET_NUM(opacity);
+	
+}
+
+
+JS_METHOD(setWindowOpacity) { NAPI_ENV; THIS_WINDOW;
+	
+	REQ_FLOAT_ARG(1, opacity);
+	
+	glfwSetWindowOpacity(window, opacity);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(maximizeWindow) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwMaximizeWindow(window);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(focusWindow) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwFocusWindow(window);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(requestWindowAttention) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwRequestWindowAttention(window);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getWindowMonitor) { NAPI_ENV; THIS_WINDOW;
+	
+	GLFWmonitor *monitor = glfwGetWindowMonitor(window);
+	
+	if ( ! monitor ) {
+		RET_NULL;
+	}
+	
+	GLFWmonitor *primary = glfwGetPrimaryMonitor(window);
+	
+	RET_VALUE(describeMonitor(env, monitor, primary ? true : monitor == primary));
 	
 }
 
@@ -746,6 +921,247 @@ JS_METHOD(extensionSupported) { NAPI_ENV;
 	REQ_STR_ARG(0, str);
 	
 	RET_BOOL(glfwExtensionSupported(str.c_str()) == 1);
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+JS_METHOD(setWindowAttrib) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwSetWindowAttrib(GLFWwindow* window, int attrib, int value);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(waitEventsTimeout) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwWaitEventsTimeout(double timeout);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(postEmptyEvent) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwPostEmptyEvent(void);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getInputMode) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetInputMode(GLFWwindow* window, int mode);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(rawMouseMotionSupported) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwRawMouseMotionSupported(void);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getKeyName) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetKeyName(int key, int scancode);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getKeyScancode) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetKeyScancode(int key);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(createCursor) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwCreateCursor(const GLFWimage* image, int xhot, int yhot);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(createStandardCursor) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwCreateStandardCursor(int shape);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(destroyCursor) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwDestroyCursor(GLFWcursor* cursor);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(setCursor) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwSetCursor(GLFWwindow* window, GLFWcursor* cursor);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getJoystickHats) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetJoystickHats(int jid, int* count);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getJoystickGUID) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetJoystickGUID(int jid);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(joystickIsGamepad) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwJoystickIsGamepad(int jid);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(updateGamepadMappings) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwUpdateGamepadMappings(const char* string);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getGamepadName) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetGamepadName(int jid);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getGamepadState) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetGamepadState(int jid, GLFWgamepadstate* state);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(setClipboardString) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwSetClipboardString(GLFWwindow* window, const char* string);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getClipboardString) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetClipboardString(GLFWwindow* window);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getTimerValue) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetTimerValue(void);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getTimerFrequency) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetTimerFrequency(void);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getProcAddress) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetProcAddress(const char* procname);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(vulkanSupported) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwVulkanSupported(void);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getRequiredInstanceExtensions) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetRequiredInstanceExtensions(uint32_t* count);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getInstanceProcAddress) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetInstanceProcAddress(VkInstance instance, const char* procname);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(getPhysicalDevicePresentationSupport) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwGetPhysicalDevicePresentationSupport(VkInstance instance, VkPhysicalDevice device, uint32_t queuefamily);
+	RET_UNDEFINED;
+	
+}
+
+
+JS_METHOD(createWindowSurface) { NAPI_ENV; THIS_WINDOW;
+	
+	glfwCreateWindowSurface(VkInstance instance, GLFWwindow* window, const VkAllocationCallbacks* allocator, VkSurfaceKHR* surface);
+	RET_UNDEFINED;
 	
 }
 
