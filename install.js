52a82220fbcbeb8ed0882b4bd361420275544cd5
Luis Blanco <luisblanco1337@gmail.com>
2024-11-03 00:27:13 +0400

    Update install link

--- install.js
+++ install.js
@@ -4,6 +4,6 @@ const { install } = require('addon-tools-raub');
 
 
 const prefix = 'https://github.com/node-3d/glfw-raub/releases/download';
-const tag = '5.5.0';
+const tag = '6.0.0';
 
 install(`${prefix}/${tag}`);
